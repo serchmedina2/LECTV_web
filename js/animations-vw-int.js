0659104d22e6716d1af0c602a0c25a0dcb0adee2
Sergio Javier <320334908@alumno.enp.unam.mx>
2022-06-28 11:23:05 -0500

    Agregadas animaciones a seccion de noticias

--- js/animations-vw-int.js
+++ js/animations-vw-int.js
@@ -1,13 +1,30 @@
 // Version 2.0 
-const observer = new IntersectionObserver(entries => {
-  entries.forEach((entry) =>{
-    const banners = entry.target.querySelectorAll('.banner');
-    if(entry.isIntersecting){
-      banners.forEach((banner) => banner.classList.add('animate__slideInUp'));
-      return;
-    }  
-    banners.forEach((banner) => banner.classList.remove('animate__slideInUp'));
+/**
+ *  A function that adds an Animate CSS' class to 
+ *  a specific class of the elements inside of it
+ *  in a container selected, when the container
+ *  is visible on screen.
+ *  It has 3 arguments parent, the parent container element
+ *  elementClass, the class we want to animate and the
+ *  animation name. This can work with personalized css
+ *  animations too.
+ * */
+function scrollAnimation(parent, elementClass, animation) {
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach((entry) =>{
+      const banners = entry.target.querySelectorAll(elementClass);
+      if(entry.isIntersecting){
+        banners.forEach((banner) => banner.classList.add(animation));
+        return;
+      }
+      banners.forEach((banner) => banner.classList.remove(animation));
+    });
   });
-});
+  const parentElements = document.querySelectorAll(parent);
+  parentElements.forEach((parentE) => observer.observe(parentE));
+}
 
-observer.observe(document.querySelector('.banner-container'));
+scrollAnimation('.banner-container', '.banner', 'animate__backInUp');
+
+scrollAnimation('.news-grid', '.new-container', 'animate__bounceIn')
+
