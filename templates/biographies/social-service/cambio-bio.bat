632fd5f9728986a09471402408d5a34444e745fb
Sergio Javier <320334908@alumno.enp.unam.mx>
2022-08-04 17:00:10 -0500

    Ordenadas las entradas de semblanzas personales en uss categorias correctas, sgregada categoria de voluntarios y colaboradores al navbar y agregados los servicios sociales.

--- templates/biographies/social-service/cambio-bio.bat
+++
@@ -1,5 +0,0 @@
-Get-ChildItem .\*.html -Recurse | ForEach { (Get-Content $_ | ForEach { $_ -replace '                <div class="bio-title">Biografía y Formación Académica</div>
-                <!--Animacion 7 clase de fuente / color de fondo -->
-                <div class="bio-content bio-typo">', '                <div class="bio-content bio-typo">
-                    <div class="bio-title">Biografía y Formación Académica</div>' }) | Set-Content $_ }
-
